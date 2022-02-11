2cbdbd352b55d663130b385851146570610e5557
Joise Garcia <alejandrogar17@gmail.com>
2022-02-11 01:04:50 -0300

    Android configurations

--- src/hooks/useLocale.js
+++ src/hooks/useLocale.js
@@ -1,8 +1,9 @@
 import {NativeModules, Platform} from 'react-native';
 
 export default function useLocale() {
-  const locale = Platform.OS
-    ? NativeModules.SettingsManager.settings.AppleLocale
-    : NativeModules.I18nManager.localeIdentifier;
+  const locale =
+    Platform.OS === 'ios'
+      ? NativeModules.SettingsManager.settings.AppleLocale
+      : NativeModules.I18nManager.localeIdentifier;
   return `${locale}`.replace('_', '-');
 }
